bbef2ffe350a59aafc4cfa85b1e711ad9cce415f
stanis <stanis@netvolution.fr>
2023-04-19 14:24:46 +0200

    Récupération des données sur API JSON

--- app/src/main/java/com/example/listnews/MainActivity.kt
+++ app/src/main/java/com/example/listnews/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.listnews
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -18,9 +20,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import com.example.listnews.ui.theme.ListNewsTheme
+import org.json.JSONArray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +50,11 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainList() {
+    val context = LocalContext.current
+    val response = remember { mutableStateOf<JSONArray?>(null) }
+
+    fetchPosts(context, response)
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -55,10 +68,14 @@ fun MainList() {
         LazyColumn(
             contentPadding = padding
         ) {
-            items(100) {
+            items(response.value?.length() ?: 0) {index ->
+                val post = response.value?.getJSONObject(index)
                 ListItem(
-                    headlineText = { Text("One line list item with 24x24 icon") },
                     leadingContent = {
+                        Text((index + 1).toString())
+                    },
+                    headlineText = { Text(post?.getString("title") ?:"") },
+                    trailingContent = {
                         Icon(
                             Icons.Filled.ArrowRight,
                             contentDescription = "Localized description",
@@ -77,4 +94,19 @@ fun GreetingPreview() {
     ListNewsTheme {
         MainList()
     }
+}
+
+fun fetchPosts(context: Context, response: MutableState<JSONArray?>) {
+    val url = "https://jsonplaceholder.typicode.com/posts"
+    val queue = Volley.newRequestQueue(context)
+
+    val jsonArrayRequest = JsonArrayRequest(
+        Request.Method.GET, url, null, {resp ->
+            response.value = resp
+
+        }, {
+            Log.e("Volley", "Error: ${it.message}")
+        }
+    )
+    queue.add(jsonArrayRequest)
 }
